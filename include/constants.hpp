a0c4a2cc1e78caed73cdce934923e07a412b9a5e
JakubRed <jakubredmerski@gmail.com>
2024-04-07 22:56:03 +0200

    Expand readme, remove unreliable library

--- include/constants.hpp
+++ include/constants.hpp
@@ -4,15 +4,13 @@
 #include <iostream>
 #include <fstream>
 #include <fcntl.h>
-#include <sys/stat.h> 
 
 const int MINIMAL_VALID_VALUE_OF_ARGUMENTS = 4;
 const int ACCESS_OPTION = 1;
 const int FILE_NAME = 2;
 const int FILE_FUNC_ERROR = -1;
 const int THINGS_OTHER_THAN_WORDS = 3;
-// const int ALL_PERMISSIONS = 0x0080| (0x0080 >> 3)  | ((0x0080 >> 3) >> 3);
-const int ALL_PERMISSIONS = S_IWUSR | S_IWGRP | S_IWOTH;
+const int ALL_PERMISSIONS = 0x0080| (0x0080 >> 3)  | ((0x0080 >> 3) >> 3); // S_IWUSR | S_IWGRP | S_IWOTH;
 
 enum class ErrorCodes {
     NO_ERROR = 0,
